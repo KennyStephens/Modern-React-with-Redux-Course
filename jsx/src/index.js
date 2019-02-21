847bd4f42679353d2605968f26b29bbd19b280c3
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-02-21 07:58:36 -0700

    Inline styling with jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,11 +6,13 @@ import ReactDOM from "react-dom";
 const App = () => {
   return (
     <div>
-      <label class='label' for='name'>
+      <label className='label' for='name'>
         Enter Name:
       </label>
       <input id='name' type='text' />
-      <button style='background-color: blue; color: white;'>Submit</button>
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
     </div>
   );
 };
