1bb2e7ffd455bf1465c26cdd51eeae050f8dfb84
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-02-27 09:20:58 -0700

    Added a loading spinner

--- seasons-class-based-components/src/index.js
+++ seasons-class-based-components/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 // Functional Component
 // const App = () => {
@@ -39,7 +40,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
