b696069ecbba3e0b0cbc4eba3591d59bbde0fe82
Kenny Stephens <kennethcstephens@gmail.com>
2019-03-01 20:07:57 -0700

    thing

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   onSearchSubmit(term) {
     console.log(term);
   }
-
+  
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
