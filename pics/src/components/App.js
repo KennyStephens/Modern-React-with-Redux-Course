f771c2a5c6d4bde952d0ae8ed0e9eff2a393edd8
Kenny Stephens <kennethcstephens@gmail.com>
2019-03-02 16:26:43 -0700

    Setting state

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,6 +4,8 @@ import SearchBar from "./SearchBar";
 
 
 class App extends React.Component {
+  state = { images: [] };
+
   async onSearchSubmit(term) {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
@@ -11,14 +13,16 @@ class App extends React.Component {
         Authorization: 'Client-ID 22d217e0ebeba6b679983d65ae35473c2f216302d4b37fd6536149742b80e34a'
       }
     });
+
+    this.setState({ images: response.data.results });
     
-    console.log(response.data.results);
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
           <SearchBar onSubmit={this.onSearchSubmit} />
+          Found: {this.state.images.length} Images
       </div>
     );
   }
