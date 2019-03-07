1ad868b5ec046840914dcf39730229180eee439a
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-03-07 08:35:23 -0700

    Setting up API

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBn12t3K5PatfBGAX9AdRRFD4JGSbgViX8';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
