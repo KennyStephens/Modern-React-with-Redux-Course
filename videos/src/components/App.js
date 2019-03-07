7d0f7f862cc93cdbde8e082fcf944aeb52c863d9
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-03-07 08:55:06 -0700

    Making requets to Youtube API

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="ui container"><SearchBar /></div>
-        );
-    }
+  onTermSubmit = term => {
+    youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className='ui container'>
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
