f7fafa9c38767d99b32cab913e9aacb7c1497f76
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-03-12 08:44:08 -0600

    Displaying song details

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-const SongDetail = (props) => {
-    console.log(props);
-    return <div>Song detail</div>
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Please select a song.</div>;
+  }
+
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>Title: {song.title} </p>
+      <p>Duration: {song.duration} </p>
+    </div>
+  );
 };
 
-const mapStateToProps = (state) => {
-    return { song:state.selectedSong }
+const mapStateToProps = state => {
+  return { song: state.selectedSong };
 };
 
 export default connect(mapStateToProps)(SongDetail);
